59f006d10c314faefed0a0a8a953a8bd3f5dd27f
Jisell Howe <102433478+ji53ll@users.noreply.github.com>
2023-12-28 12:37:22 -0600

    Add files via upload

--- goodreads_app.py
+++ goodreads_app.py
@@ -125,6 +125,9 @@ books_publication_year.columns = ['Year Published','Count']
 
 # Merge with all_titles using index-based merge
 books_publication_year = pd.merge(books_publication_year, all_titles, how='left', left_on=books_publication_year.index, right_on=all_titles.index)
+# Drop rows with NaN values in the "Title" column
+books_publication_year = books_publication_year.dropna(subset=['Title'])
+
 
 fig_year_published = px.bar(
                             books_publication_year,
