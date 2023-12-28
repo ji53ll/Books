ff546a9171cc4c58c5793969949c0c6cb5f31f01
Jisell Howe <102433478+ji53ll@users.noreply.github.com>
2023-12-28 12:34:51 -0600

    Add files via upload

--- goodreads_app.py
+++ goodreads_app.py
@@ -121,13 +121,16 @@ all_titles = aggregate_all_titles(books_df, 'Title')
 
 books_publication_year = books_df.groupby('Original Publication Year')['Book Id'].count().reset_index()
 books_publication_year.columns = ['Year Published','Count']
-books_publication_year['Title'] = books_df['Title']
-print(books_publication_year)
+#books_publication_year['Title'] = books_df['Title']
+
+# Merge with all_titles using index-based merge
+books_publication_year = pd.merge(books_publication_year, all_titles, how='left', left_on=books_publication_year.index, right_on=all_titles.index)
+
 fig_year_published = px.bar(
                             books_publication_year,
                             x='Year Published',
                             y='Count',
-                            hover_data=['Title']
+                            hover_data=['All Titles']
                             )
 fig_year_published.update_xaxes(range=[1980,2023])
 fig_year_published.update_layout(yaxis_title='')
